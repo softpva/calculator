1bb7eb623598a6eb30378edfef0c03e74e40ec10
Pedro Vitor Abreu <soft.eng@gmail.com>
2023-01-23 17:41:11 -0300

    Working in script.js

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-import Calculator from "./calulator";
+class Calculator{
+    
+    constructor(prevOpTxt,currOpTxt){
+        this.clear();
+        this.prevOpTxt = prevOpTxt;
+        this.currOpTxt = currOpTxt;
+        this.currOp = '';        
+    }       
+
+    store(){}
+
+    restore(){}
+
+    clear(){
+        this.currOpTxt = '';
+        this.prevOpTxt = '';
+        this.operation = undefined;
+    }
+
+    delete(){}
+
+    appNum(num){
+        if (num === '.' && this.currOp.includes('.')) return;
+        this.currOp = this.currOp.toString() + num.toString();
+        console.log(this.currOp);
+    }    
+
+    chooseOp(op){}
+
+    calculate(){}
+
+    show(){
+        this.currOpTxt.innerText = this.currOp;
+    }
+}
+
 
 const numBt = document.querySelectorAll("[data-num]");
 const opBt = document.querySelectorAll("[data-op]");
@@ -10,11 +45,13 @@ const delBt = document.querySelector("[data-del]");
 const prevOpTxt = document.querySelector("[data-prev]");
 const currOpTxt = document.querySelector("[data-curr]");
 
+
+
 const calc = new Calculator(prevOpTxt,currOpTxt);
 
 numBt.forEach(button => {
     button.addEventListener('click',() => {
-        calc.appNum(button.innerHTML);
+        calc.appNum(button.textContent);
         calc.show();
     })
 });
