84504f3ed7cac0784b4ed79802f1ec1f31f465ef
Pedro Vitor Abreu <soft.eng@gmail.com>
2023-02-03 11:49:57 -0300

    Refactor all files

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ class Calculator {
     e_memory = document.querySelector("[data-mr]");
     s_expression = '';
     s_number = '0';
-    n_countParenth = 0;
     s_memory = '0';
 
     constructor() {
@@ -13,33 +12,38 @@ class Calculator {
     }
 
     buildNumber(num) {
+        if (this.s_expression.includes('=') && this.s_number[0] !== '0') this.s_number = '0';
         if (this.s_number.includes('.') && num === '.') return;
-        if (this.s_number[0] === '0') this.s_number = '';
-        this.s_number += num;        
+        if (this.s_number[0] == '0') this.s_number = '';
+        this.s_number += num;
         this.show();
-        console.log("Number: " + this.s_number);
     }
 
-    buildExpression(op) {        
+    buildExpression(op) {
         if (this.s_expression.includes('=')) this.s_expression = '';
-        let c_penult = this.s_expression.slice(-2, -1);
-        if (c_penult === ')' && this.n_countParenth !== '0') this.s_expression+= '* ';
-        if (this.s_number[0] !== '0') this.s_expression += (this.s_number + ' ');        
-        c_penult = this.s_expression.slice(-2, -1);
-        if (isNaN(c_penult) && (op != '(' && op != ')')) return; // <----
-        if (!isNaN(c_penult) && op == '(') op = '* (';
-        if (c_penult === ')')
-        console.log("Parenth before: " + this.n_parenthesis());
-        if (op == ')' && this.n_parenthesis() === 0) return;    
+        let c_penult = this.s_expression.at(-2) || '';
+        // console.log("Expr: " + this.s_expression);
+        // console.log(c_penult + ' ' + this.s_number[0] + ' ' + op);
+        if (c_penult === ')' && this.s_number[0] !== '0') this.s_expression += '* ';
+        if (this.s_number[0] !== '0') this.s_expression += (this.s_number + ' ');
+        c_penult = this.s_expression.at(-2) || '';
+        if (!isNaN(c_penult) && this.s_expression !== '' && op == '(') op = '* (';
+        if (op == ')' && this.n_parenthesis() === 0) return;
         this.s_expression += (op + ' ');
         this.s_number = '0';
         this.show();
-        console.log("Expr: " + this.s_expression);   
-        console.log("Parenth after: " + this.n_parenthesis());     
     }
 
-    n_parenthesis() {
-        return ((this.s_expression.match(/\(/g) || []).length - (this.s_expression.match(/\)/g) || []).length);
+    equalPressed() {
+        if (this.s_expression.at(-2) === ')' && this.s_number[0] !== '0') this.s_expression += '* ';
+        if (this.s_number[0] !== '0') this.s_expression += (this.s_number + ' ');
+        const n_parenthesis = this.n_parenthesis();
+        if (n_parenthesis > 0)
+            for (let i = 0; i < n_parenthesis; i++) this.s_expression += ') ';
+        console.log("After Equal: " + this.s_expression);
+        this.s_number = eval(this.s_expression).toString();
+        this.s_number = parseFloat(parseFloat(this.s_number).toPrecision(15)).toString();
+        this.s_expression += '= ';
     }
 
     doCommand(com) {
@@ -48,20 +52,14 @@ class Calculator {
         if (com === 'DEL') this.delete();
         if (com === 'MS') this.setMemory();
         if (com === '+/-') this.changeSignal();
+        if (com.toString().includes('MR')) this.s_number = this.s_memory; // <----
+        // if (com.slice(0,2) === 'MR') console.log('oi'); // <----
+        // console.log(com);
         this.show();
     }
 
-    equalPressed() {
-        if (this.s_number === '0') this.s_number = '';
-        this.s_expression += (this.s_number + ' ');
-        if (this.n_countParenth > 0)
-            for (let i = 0; i < this.n_countParenth; i++) this.s_expression += ') ';
-        this.n_countParenth = 0;
-        console.log("After Equal: " + this.s_expression);
-        this.s_number = eval(this.s_expression).toString();
-        this.s_number = parseFloat(parseFloat(this.s_number).toPrecision(15));
-        this.s_expression += '= ';
-        return;
+    n_parenthesis() {
+        return ((this.s_expression.match(/\(/g) || []).length - (this.s_expression.match(/\)/g) || []).length);
     }
 
     changeSignal() {
@@ -76,27 +74,23 @@ class Calculator {
         }
         this.s_memory = this.s_number;
         this.e_memory.innerHTML = `MR<br/><div class="mem">${this.s_memory.slice(0, 10)}</div>`;
+        this.s_number = '0';
     }
 
     clearAll() {
         this.s_expression = '';
         this.s_number = '0';
-        this.n_countParenth = 0;
         return;
     }
 
     delete() {
         if (this.s_number !== '0') {
-            this.s_number = this.s_number.slice(0, this.s_number.length - 1);
+            this.s_number = this.s_number.slice(0, -1);
             if (this.s_number === '') this.s_number = '0';
             return;
         }
-        if (this.s_expression !== '') {
-            if (this.s_expression[this.s_expression.length - 1] === '=') {
-                this.s_expression = this.s_expression.slice(0, this.s_expression.length - 2);
-            }
-            this.s_expression = this.s_expression.slice(0, this.s_expression.length - 1);
-        }
+        if (this.s_expression !== '') this.s_expression = this.s_expression.slice(0, -2);
+
     }
 
     show() {
@@ -116,6 +110,11 @@ class Calculator {
             })
         })
         document.querySelectorAll("[data-com]").forEach(button => {
+            button.addEventListener('click', () => {
+                this.doCommand(button.textContent);
+            })
+        })
+        document.querySelectorAll("[data-mr]").forEach(button => {
             button.addEventListener('click', () => {
                 this.doCommand(button.textContent);
             })
